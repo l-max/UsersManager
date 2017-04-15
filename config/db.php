1ea76bf5ad681897b0339988d19a85af3bb04c10
N1 <n1@n1-Lenovo.(none)>
2017-04-15 20:54:04 +0700

    Add files

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=Test_Web',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
