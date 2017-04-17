dc1e78a054d343d1a6bf625f323703cbf7877656
N1 <n1@n1-Lenovo.(none)>
2017-04-17 12:18:41 +0700

    Delete old files

--- modules/admin/views/users/index.php
+++ modules/admin/views/users/index.php
@@ -1,17 +1,47 @@
 <?php
 
+use yii\helpers\Html;
 use yii\grid\GridView;
+use app\modules\admin\models\Status;
 
-$this->title = 'Пользователи <- Администрирование';
+/* @var $this yii\web\View */
+/* @var $searchModel app\modules\admin\models\UsersSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<p>Hello!!!</p>
+<div class="users-index">
 
-<?= GridView::widget([
-    'dataProvider' => $dataProvider,
-    'columns' => [
-        'id',
-        'name',
-        'created_at:datetime',
-    ],
-]) ?>
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
+    <p>
+        <?= Html::a('Create Users', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'login',
+            'password',
+            'name',
+            'last_name',
+            'date_reg',
+            [
+				'attribute' => 'id_status',
+				'format' => 'text',
+				'content' => function($data) {
+					return $data->status->getStatusName();
+				},
+				'filter' => Status::getStatusList()
+			],
+            'descript:ntext',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
